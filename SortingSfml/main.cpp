8f1c810e0d194b46723816d49b5bf251709f2699
Lukas Innerhofer <lukas.ihofer@hotmail.com>
2020-10-11 14:42:09 +0200

    Creating SFML window

--- SortingSfml/main.cpp
+++ SortingSfml/main.cpp
@@ -1,22 +1,66 @@
 #include "sorting.h"
+#include "SFML/Graphics.hpp"
 
 #include <iostream>
 #include <vector>
+#include <thread>
+
+template <typename RandomIt>
+void drawBarGraph(sf::RenderTarget& target, RandomIt begin, RandomIt end)
+{
+	static sf::RectangleShape rect;
+
+	const size_t dataSize = end - begin;
+	const decltype(*(std::declval<RandomIt>())) max = *std::max_element(begin, end);
+
+	rect.setFillColor(sf::Color::White);
+
+	for (RandomIt it = begin; it != end; ++it)
+	{
+		rect.setSize(
+			{ 
+				static_cast<float>(target.getSize().x) / dataSize, 
+				(static_cast<float>(*it) / max) * target.getSize().y
+			});
+		rect.setPosition(
+			{
+				(static_cast<float>(it - begin) / dataSize) * target.getSize().x,
+				static_cast<float>(target.getSize().y - rect.getSize().y)
+			});
+		target.draw(rect);
+	}
+}
 
 int main()
 {
 	std::vector<int> v{ 111, 10, 1, 4 };
 
-	sorting::bubbleSort(v.begin(), v.end(),
-		std::function<void(decltype(v.begin()), decltype(v.end()))>(
-			[](std::vector<int>::iterator begin, std::vector<int>::iterator end) -> void
+	sf::RenderWindow window;
+	window.create(sf::VideoMode(800, 600), "Sorting");
+	sf::Event e;
+	bool redraw = true;
+
+	while (window.isOpen())
+	{
+		while (window.pollEvent(e))
+		{
+			switch (e.type)
 			{
-				for (std::vector<int>::iterator it = begin; it != end; ++it)
-				{
-					std::cout << *it << ", ";
-				}
-				std::cout << "\n";
-			}));
+			case sf::Event::Closed:
+				window.close();
+				break;
+			}
+		}
+
+		if (redraw)
+		{
+			redraw = false;
+			window.clear();
+			
+			drawBarGraph(window, v.cbegin(), v.cend());
+			window.display();
+		}
+	}
 
 	return 0;
 }
